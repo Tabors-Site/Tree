1686b654f3a9fa02bd9645175b7b07936db27c56
taborgreat <taborgreat@gmail.com>
2026-01-05 10:24:05 -0800

    rate limiting on login routes

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -13,13 +13,56 @@ import authenticate from "../middleware/authenticate.js";
 
 const router = express.Router();
 
-router.post("/register", register);
+import rateLimit from "express-rate-limit";
 
-router.post("/login", login);
+const registerLimiter = rateLimit({
+  windowMs: 60 * 60 * 1000,
+  max: 5,
+  handler: (req, res) => {
+    res.status(429).json({
+      error: "Too many registrations",
+      message: "Please try again in 1 hour.",
+    });
+  },
+});
+
+// Limit login attempts
+const loginLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 10,
+  handler: (req, res) => {
+    res.status(429).json({
+      error: "Too many login attempts",
+      message: "You have been blocked for 15 minutes due to spam.",
+      retryAfterMinutes: 15,
+    });
+  },
+});
+
+// Limit email-based actions
+const emailLimiter = rateLimit({
+  windowMs: 60 * 60 * 1000,
+  max: 3,
+  handler: (req, res) => {
+    res.status(429).json({
+      error: "Too many email requests",
+      message: "Too many email requests.",
+    });
+  },
+});
+
+router.post("/register", registerLimiter, register);
+
+router.post("/login", loginLimiter, login);
 
 router.post("/logout", authenticate, logout);
 
-router.post("/setHTMLShareToken", authenticate, setHtmlShareToken);
+router.post(
+  "/setHTMLShareToken",
+  authenticate,
+  loginLimiter,
+  setHtmlShareToken
+);
 
 router.post("/verify-token", authenticate, getHtmlShareToken, (req, res) => {
   res.json({
@@ -29,7 +72,7 @@ router.post("/verify-token", authenticate, getHtmlShareToken, (req, res) => {
   });
 });
 
-router.post("/user/forgot-password", forgotPassword);
+router.post("/user/forgot-password", emailLimiter, forgotPassword);
 router.post("/user/reset-password", resetPassword);
 router.get("/user/verify/:token", verifyEmail);
 
